b7446ee678f2d7333a8359d7aec4478a8fa88a28
Simon Iversen <simo002m@gmail.com>
2017-07-18 14:39:35 +0200

    made a feature in gcryptor.py to browse the filesystem easier and when a file has been picked a Label that displays the file that has been picked(full path, eg /home/user/Desktop/file), also commented Linux_Install_Script.sh

--- Linux_Install_Script.sh
+++ Linux_Install_Script.sh
@@ -1,15 +1,18 @@
 #Copy the file to /usr/local/bin and change permissions to match the other files 
-#in that directory
+#in that directory.
 sudo cp gcryptor.py /usr/local/bin/gcryptor
 sudo chown root:root /usr/local/bin/gcryptor
 sudo chmod a-rwx /usr/local/bin/gcryptor
 sudo chmod a+rx /usr/local/bin/gcryptor
 sudo chmod u+w /usr/local/bin/gcryptor
 
+#Make a .desktop file so it's easily accesable in the menu and will appear as an icon.
+#Then copy it to /usr/share/applications and change permissions
 sudo cp gcryptor.desktop /usr/share/applications
 sudo chown root:root /usr/share/applications/gcryptor.desktop
 sudo chmod a-rwx /usr/share/applications/gcryptor.desktop
 sudo chmod a+r /usr/share/applications/gcryptor.desktop
 sudo chmod u+w /usr/share/applications/gcryptor.desktop
 
+#Copy the image into /usr/share/icons
 sudo cp gcryptor_icon.gif /usr/share/icons
--- gcryptor.py
+++ gcryptor.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
+from tkinter import filedialog
 from tkinter import font
 from tkinter import *
 
 window = Tk()
 window.title("GCryptor")
-window.geometry("1250x500+70+110")
+window.geometry("900x500+230+110")
 
 img = Image("photo", file="/usr/share/icons/gcryptor_icon.gif")
 window.tk.call("wm","iconphoto",window._w,img)
@@ -13,14 +14,14 @@ window.tk.call("wm","iconphoto",window._w,img)
 def encrypt(filename): 
     encrypted = ""
 
-    with open(str(filename.get())) as file:
+    with open(str(filename)) as file:
         for line in file:
             for letter in line:
                 encrypted += chr(ord(letter) + 5)
 
     final_encryption = encrypted * 3
 
-    file = open(str(filename.get()),"w")
+    file = open(str(filename),"w")
     file.write(final_encryption)
     file.close()
 
@@ -28,14 +29,14 @@ def encrypt(filename):
 def decrypt(filename): 
     decrypted = ""
 
-    with open(str(filename.get())) as file:
+    with open(str(filename)) as file:
         for line in file:
             for letter in line:
                 decrypted += chr(ord(letter) - 5)
 
     final_decryption = decrypted[0:int(len(decrypted) / 3)]
 
-    file = open(str(filename.get()),"w")
+    file = open(str(filename),"w")
     file.write(final_decryption)
     file.close()
 
@@ -47,6 +48,7 @@ def main_menu():
     def destroy_main():
         main.destroy()
 
+    i10 = font.Font(family="impact", size=10)
     i11 = font.Font(family="impact", size=11)
     i12 = font.Font(family="impact", size=12)
     i12bold = font.Font(family="impact", size=12, weight=font.BOLD)
@@ -62,15 +64,22 @@ def main_menu():
 
         back = Button(encrypt_frame, height=4, width=10, bg="#a7a7a7", font=i12 , text="Back", command=lambda:[destroy_encrypt(), main_menu()]).grid(row=0, column=0)
 
-        spacer = Frame(encrypt_frame, width=270).grid(row=0, column=1)
+        spacer1 = Frame(encrypt_frame, width=100).grid(row=0, column=1)
 
-        filename = StringVar()
+        def browse_filesystem():
+            global filename
+            filename = filedialog.askopenfilename()
 
-        entry_box = Entry(encrypt_frame, font=i15, textvariable=filename)
-        entry_box.grid(row=0, column=2)
+            spacer2 = Frame(encrypt_frame, width=45).grid(row=0, column=4)
+            
+            show_picked_file = Label(encrypt_frame, height=2, width=30, anchor="w", bg="#a7a7a7", font=i11, text=filename)
+            show_picked_file.grid(row=0, column=5, sticky=N)
+
+        browse_button = Button(encrypt_frame, height=2, width=15, bg="#a7a7a7", font=i12, text="Look For The File", command=browse_filesystem)
+        browse_button.grid(row=0, column=2, sticky=N)
 
         do_encryption = Button(encrypt_frame, bg="#e49400", height=2, width=10, font=i12bold, text="Encrypt", command=lambda:encrypt(filename))
-        do_encryption.grid(row=0, column=3)
+        do_encryption.grid(row=0, column=3, sticky=N)
 
     def decrypt_menu():
         decrypt_frame = Frame()
@@ -81,27 +90,35 @@ def main_menu():
 
         back = Button(decrypt_frame, height=4, width=10, bg="#a7a7a7", font=i12 , text="Back", command=lambda:[destroy_decrypt(), main_menu()]).grid(row=0, column=0)
 
-        spacer = Frame(decrypt_frame, width=270).grid(row=0, column=1)
+        spacer1 = Frame(decrypt_frame, width=100).grid(row=0, column=1)
 
-        filename = StringVar()
+        def browse_filesystem():
+            global filename
+            filename = filedialog.askopenfilename()
 
-        entry_box = Entry(decrypt_frame, font=i15, textvariable=filename)
-        entry_box.grid(row=0, column=2)
+            spacer2 = Frame(decrypt_frame, width=45).grid(row=0, column=4)
+            
+            show_picked_file = Label(decrypt_frame, height=2, width=30, bg="#a7a7a7", font=i11, text=filename)
+            show_picked_file.grid(row=0, column=5, sticky=N)
 
-        do_decryption = Button(decrypt_frame, bg="#e49400", height=2, width=10, font=i12bold, text="Decrypt", command=lambda:decrypt(filename))
-        do_decryption.grid(row=0, column=3)
-    
-    
-    text = Label(main, height=5, bg="#a7a7a7", font=i11, text="""GCryptor - An easy-to-use graphical tool for encrypting and decrypting a file.
+        browse_button = Button(decrypt_frame, height=2, width=15, bg="#a7a7a7", font=i12, text="Look For The File", command=browse_filesystem)
+        browse_button.grid(row=0, column=2, sticky=N)
         
-Note: A file that has been encrypted by this tool can only be decrypted by this tool and files text that haven't been encrypted by this tool can't be decrypted by it.""")
-    text.pack(fill=X)
+        do_decryption = Button(decrypt_frame, bg="#e49400", height=2, width=10, font=i12bold, text="Decrypt", command=lambda:decrypt(filename))
+        do_decryption.grid(row=0, column=3, sticky=N)
+    
+    
+    text = Label(main, height=5, width=90, bg="#a7a7a7", font=i11, text="""GCryptor - An easy-to-use graphical tool for encrypting and decrypting a file.
+        
+Note: A file that has been encrypted by this tool can only be decrypted by this tool and files that haven't
+been encrypted by this tool can't be decrypted by it.""")
+    text.pack()
 
     container_for_buttons = Label(main)
     container_for_buttons.pack()
 
-    encryption_button = Button(container_for_buttons, height=5, width=55, bg="#e49400", font=i12bold ,text="Encryption", command=lambda:[destroy_main(), encrypt_menu()]).pack(side=LEFT) 
-    decryption_button = Button(container_for_buttons, height=5, width=55, bg="#e49400", font=i12bold, text="Decryption", command=lambda:[destroy_main(), decrypt_menu()]).pack(side=LEFT) 
+    encryption_button = Button(container_for_buttons, height=5, width=38, bg="#e49400", font=i12bold ,text="Encryption", command=lambda:[destroy_main(), encrypt_menu()]).pack(side=LEFT) 
+    decryption_button = Button(container_for_buttons, height=5, width=38, bg="#e49400", font=i12bold, text="Decryption", command=lambda:[destroy_main(), decrypt_menu()]).pack(side=LEFT) 
 
 main_menu()
 window.mainloop()
